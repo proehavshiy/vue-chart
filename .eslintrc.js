01f1f822752db6d62e20560bb9f39bf8cbd21baa
Egor Kurkin <proehavshiynapravo@yandex.ru>
2022-06-23 22:13:16 +0300

    refactor: store

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,9 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'import/prefer-default-export': [
+      'off',
+    ],
     'no-unused-expressions': [
       'error',
       {
--- src/store/modules/analyticsModule.js
+++ src/store/modules/analyticsModule.js
@@ -1,5 +1,7 @@
-/* eslint-disable import/prefer-default-export */
 import axios from 'axios';
+import CONSTANTS from '@/constants/constants';
+
+const { RESPONSE_URL, API_KEY, ANALYTICS_DATA } = CONSTANTS;
 
 export const analyticsModule = {
   state: () => ({
@@ -7,37 +9,7 @@ export const analyticsModule = {
       id: '',
     },
     siteId: '',
-    analyticsData: [
-      { date: '2020-07-01', visits: 213 },
-      { date: '2020-07-02', visits: 249 },
-      { date: '2020-07-03', visits: 179 },
-      { date: '2020-07-04', visits: 170 },
-      { date: '2020-07-05', visits: 184 },
-      { date: '2020-07-06', visits: 202 },
-      { date: '2020-07-07', visits: 198 },
-      { date: '2020-07-08', visits: 168 },
-      { date: '2020-07-09', visits: 176 },
-      { date: '2020-07-10', visits: 171 },
-      { date: '2020-07-11', visits: 190 },
-      { date: '2020-07-12', visits: 154 },
-      { date: '2020-07-13', visits: 246 },
-      { date: '2020-07-14', visits: 250 },
-      { date: '2020-07-15', visits: 227 },
-      { date: '2020-07-16', visits: 140 },
-      { date: '2020-07-17', visits: 170 },
-      { date: '2020-07-18', visits: 125 },
-      { date: '2020-07-19', visits: 106 },
-      { date: '2020-07-20', visits: 207 },
-      { date: '2020-07-21', visits: 222 },
-      { date: '2020-07-22', visits: 198 },
-      { date: '2020-07-23', visits: 204 },
-      { date: '2020-07-24', visits: 213 },
-      { date: '2020-07-25', visits: 145 },
-      { date: '2020-07-26', visits: 166 },
-      { date: '2020-07-27', visits: 163 },
-      { date: '2020-07-28', visits: 135 },
-      { date: '2020-07-29', visits: 45 },
-    ],
+    analyticsData: [],
     isLoading: false,
   }),
   getters: {
@@ -50,7 +22,7 @@ export const analyticsModule = {
       state.siteId = value;
     },
     setAnalyticsData(state, data) {
-      state.analyticsDate = data;
+      state.analyticsData = data;
     },
     setLoading(state, bool) {
       state.isLoading = bool;
@@ -60,15 +32,20 @@ export const analyticsModule = {
     async fetchAnalyticsData({ state, commit }) {
       try {
         commit('setLoading', true);
-        const response = await axios.get('https://track-api.leadhit.io/client/test_auth', {
+        const response = await axios.get(RESPONSE_URL, {
           headers: {
-            'Api-Key': '5f8475902b0be670555f1bb3:eEZn8u05G3bzRpdL7RiHCvrYAYo',
+            'Api-Key': API_KEY,
             'Leadhit-Site-Id': state.inputData.id,
           },
         });
-        console.log('response:', response);
-        commit('setSiteId', state.inputData.id);
-        console.log('state.inputData.id:', state.inputData.id);
+        const { data: { message } } = response;
+        if (message === 'ok') {
+          // установка siteId
+          // в localStorage сохранится автоматически
+          commit('setSiteId', state.inputData.id);
+          // мок получения и записи данных для графика в стейт
+          commit('setAnalyticsData', ANALYTICS_DATA);
+        }
       } catch (e) {
         console.log('error fetch:', e);
       } finally {
@@ -77,5 +54,4 @@ export const analyticsModule = {
       }
     },
   },
-  // namespaced: true,
 };
